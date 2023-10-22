7b93bc47239295f0e23521b99329c8e7f0dee337
louisdpro <122191019+Louisdpr0@users.noreply.github.com>
2023-10-22 15:55:11 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
-const fs = require('fs')
+const menu = require('fs')
 
 global.fotomenfess = fs.readFileSync("./menfess.jpg")
-global.owner = ["6289653898291"]
+global.owner = ["62887435047326"]
 
 global.help = {
 Menu(pushname){
   return `Halo ${pushname}
 
-Saya adalah Zenibot, Ketik .list untuk menampilkan semua menu .
+Saya adalah bot WhatsApp confess 👋 ketik .confess untuk memulai chat.
 `
 }
 }
